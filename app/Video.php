758df90c05fa0c2847ce9b303b543f8d44ec2a51
FareedAlBandar <fareed@Fareed-Laptop.localdomain>
2020-07-25 19:48:51 +0300

    created not creating

--- app/Video.php
+++ app/Video.php
@@ -18,7 +18,7 @@ class Video extends Model
     protected static function boot()
     {
         parent::boot();
-        static::creating(function (Video $video) {
+        static::created(function (Video $video) {
             $file_path = Storage::disk('public')->path($video->file);
             $logo = Storage::disk('public')->path("logo/logo.png");
             shell_exec('ffmpeg -y -i '. $file_path .' -i '. $logo .' -filter_complex "[1:v][0:v]scale2ref=(252/200)*ih/8/sar:ih/8[wm][base];[base][wm]overlay=10:10" '. Storage::disk('public')->path('').'temp/'.$video->file);
