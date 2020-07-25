635ec4d11d7d7389bde1cfb6f2f6f5bf5f09c04c
FareedAlBandar <fareed@Fareed-Laptop.localdomain>
2020-07-25 18:43:02 +0300

    minor improvement

--- app/Video.php
+++ app/Video.php
@@ -20,7 +20,7 @@ class Video extends Model
         static::creating(function ($video) {
             $file_path = Storage::disk('public')->path($video->file);
             $logo = Storage::disk('public')->path("logo/logo.png");
-            shell_exec('ffmpeg -y -i '. $file_path .' -i '. $logo .' -filter_complex "overlay=10:10" '. $file_path);
+            shell_exec('ffmpeg -y -i '. $file_path .' -i '. $logo .' -filter_complex "[1:v][0:v]scale2ref=(252/200)*ih/8/sar:ih/8[wm][base];[base][wm]overlay=10:10" '. $file_path);
         });
     }
 
