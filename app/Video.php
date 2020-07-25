34b40a5145331ca3d1a58a6f723c2268a78bd3b3
FareedAlBandar <fareed@Fareed-Laptop.localdomain>
2020-07-25 19:35:59 +0300

    fix

--- app/Video.php
+++ app/Video.php
@@ -21,9 +21,7 @@ class Video extends Model
         static::creating(function (Video $video) {
             $file_path = Storage::disk('public')->path($video->file);
             $logo = Storage::disk('public')->path("logo/logo.png");
-            $ext = shell_exec('ffprobe -v error -select_streams v:0 -show_entries stream=codec_name -of default=nokey=1:noprint_wrappers=1 '.$file_path);
-            $ext = substr($ext, 0, -2);
-            shell_exec('ffmpeg -y -i '. $file_path .' -i '. $logo .' -filter_complex "[1:v][0:v]scale2ref=(252/200)*ih/8/sar:ih/8[wm][base];[base][wm]overlay=10:10" '. Storage::disk('public')->path('').'temp'.$ext);
+            shell_exec('ffmpeg -y -i '. $file_path .' -i '. $logo .' -filter_complex "[1:v][0:v]scale2ref=(252/200)*ih/8/sar:ih/8[wm][base];[base][wm]overlay=10:10" '. Storage::disk('public')->path('').'temp/'.$video->file);
             shell_exec('mv -f '.Storage::disk('public')->path('').'temp '.$file_path);
         });
     }
